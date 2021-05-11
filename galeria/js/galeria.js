27893a8fb8e184b10b5db2dc06f6d55712de0d99
Gabriel <Gabriel@gmail.com>
2021-05-11 14:02:51 -0400

    Responsive finalizado

---
+++ galeria/js/galeria.js
@@ -0,0 +1,8 @@
+
+const act = document.getElementById('menu_btn');
+const act_2 = document.getElementById('menu');
+
+function cambio() {
+    act.classList.toggle('act');
+    act_2.classList.toggle('act_2');
+};
